51b4a415096883b8d10a381b36085e2b95b147c6
HorusEY3s <380686356@qq.com>
2022-01-17 12:12:10 +0800

    add donate module

--- vue-spring-demo/springboot/src/main/java/com/example/demo/controller/RoleController.java
+++ vue-spring-demo/springboot/src/main/java/com/example/demo/controller/RoleController.java
@@ -51,15 +51,20 @@ public class RoleController  {
 
         // 2.再新增 新的绑定关系
        //role.getPermissions()是前端传过来的角色对应的资源路径，然后进行for循环
-        //role.getPermissions()得到的是Permission集合，
+        //role.getPermissions()得到的是PermissionID的集合，
+        //role.getId(),是固定的，一直是传过来role的id
         for (Integer permissionId : role.getPermissions()) {
             permissionMapper.insertRoleAndPermission(role.getId(), permissionId);
         }
 
         // 判断当前登录的用户角色是否包含了当前操作行的角色id，如果包含，则返回true，需要重新登录。
+        //获得当前登陆的user信息
         User user = TokenUtils.getUser();
+        //利用user的id获取对应的roleID，使用的表是user_role,用户可以有多个角色，所以是返回的是List
         List<UserRole> userRoles = RoleMapper.getRoleIdByUserId(user.getId());
-        if (userRoles.stream().anyMatch(userRole -> userRole.getRoleId().equals(role.getId()))) {
+        //当前用户的中的RoleID中有任何一个和前端传过来的role里面的ID一样
+        //也就是更改的是当前用户相关的角色， 返回true前端有个if判断，如果res.data=true就会跳回到login界面
+        if (userRoles.stream().anyMatch(item -> item.getRoleId().equals(role.getId()))) {
             return Result.success(true);
         }
 //        如果不包含，则返回false，不需要重新登录。
@@ -90,12 +95,25 @@ public class RoleController  {
         if (StrUtil.isNotBlank(search)) {
             wrapper.like(Role::getName, search);
         }
+
+
         Page<Role> RolePage = RoleMapper.selectPage(new Page<>(pageNum, pageSize), wrapper);
+
+        //查询当前角色拥有的访问权限
+        //获得页面的分类的结果 数据来源于role表
         List<Role> records = RolePage.getRecords();
         // 给角色设置绑定的权限id数组
         for (Role record : records) {
+            //获取当前role表每条数据的id，也就是roleId
             Integer roleId = record.getId();
-            List<Integer> permissions = permissionMapper.getPermissionByRoleId(roleId).stream().map(RolePermission::getPermissionId).collect(Collectors.toList());
+            //getPermissionByRoleId(roleId)，根据roleId获取可用的PermissionId，数据来自role_permission表
+            List<RolePermission> permissionId = permissionMapper.getPermissionByRoleId(roleId);
+            //遍历permissionId 数列，获取里面的id
+            //把遍历的值变成一个数组collect(Collectors.toList())
+            //Collectors.toList()=用来结束Stream流
+            //这里的目的是把泛型从RolePermission变成Integer类型
+            List<Integer> permissions = permissionId.stream().map(RolePermission::getPermissionId).collect(Collectors.toList());
+            //在结果中放入这个数列
             record.setPermissions(permissions);
         }
         return Result.success(RolePage);
